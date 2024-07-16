b629abb5d57b9c12561c0cc8946b139ea5990fcb
shivPosted <shivpratap621@gmail.com>
2024-07-16 19:00:56 +0530

    done with initial application of MVC architecture -applied initial phases of model and controller...

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,8 @@
-const recipeContainer = document.querySelector('.recipe-detailed-info');
+import * as model from './model.js';
 
+import icons from 'url:../img/icons.svg';
+console.log(icons);
+const recipeContainer = document.querySelector('.recipe-detailed-info');
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -16,7 +19,7 @@ const renderSpinner = function (elem) {
   const html = `
                 <div class="spinner">
         <svg class="spinner--icon">
-          <use href="src/img/icons.svg#icon-loader"></use>
+          <use href="${icons}#icon-loader"></use>
         </svg>
       </div>
       `;
@@ -24,76 +27,66 @@ const renderSpinner = function (elem) {
 };
 
 ///////////////////////////////////////
-const apiKey = '5b0f13db-34ac-41da-941f-76dbebc41504';
 // const id = '664c8f193e7aa067e94e8706';
 // const id = '664c8f193e7aa067e94e8610';
 // const id = '664c8f193e7aa067e94e8534';
+
+renderSpinner(recipeContainer);
+
 const renderRecipe = async function (search) {
   //1.loading the recipe
+
+  const apiKey = '5b0f13db-34ac-41da-941f-76dbebc41504';
   const id = window.location.hash.slice(1);
   if (!id) return;
-  renderSpinner(recipeContainer);
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=${apiKey}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message}`);
-    console.log(data);
-    const {
-      cooking_time: time,
-      image_url: img,
-      source_url: source,
-      title,
-      publisher,
-      servings,
-      ingredients,
-    } = data.data.recipe;
-    console.log(time, img, source, title, publisher, servings, ingredients);
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+    // console.log(time, img, source, title, publisher, servings, ingredients);
 
     //2. rendering the recipe
-    const ingredientList = ingredients.reduce((accum, elem) => {
+    const ingredientList = recipe.ingredients.reduce((accum, elem) => {
       let unit, quantity;
       quantity = elem.quantity ? elem.quantity : '';
       unit = elem.unit ? elem.unit : '';
       return (accum += `<li class="ingredient--list--item">
                     <svg class="check--icon">
-                      <use href="src/img/icons.svg#icon-check"></use>
+                      <use href="${icons}#icon-check"></use>
                     </svg>
                     <span>${quantity} ${unit} ${elem.description}</span>
                   </li>`);
     }, '');
 
-    console.log(ingredientList);
+    // console.log(ingredientList);
     const html = `<div class="recipe--overview">
               <figure>
-                <img src=${img} alt="${title}" />
+                <img src=${recipe.img} alt="${recipe.title}" />
                 <h1 class="recipe--title">
-                  <span>${title}</span>
+                  <span>${recipe.title}</span>
                 </h1>
               </figure>
               <div class="recipe--overview--options">
                 <div class="cooking--time">
                   <svg class="clock--icon">
-                    <use href="src/img/icons.svg#icon-clock"></use>
+                    <use href="${icons}#icon-clock"></use>
                   </svg>
-                  <span>${time}</span> Minutes
+                  <span>${recipe.time}</span> Minutes
                 </div>
                 <div class="serving--selector">
                   <svg class="user--icon">
-                    <use href="src/img/icons.svg#icon-users"></use>
+                    <use href="${icons}#icon-users"></use>
                   </svg>
-                  <span>${servings}</span> servings
+                  <span>${recipe.servings}</span> servings
                   <svg class="minus--icon">
-                    <use href="src/img/icons.svg#icon-minus-circle"></use>
+                    <use href="${icons}#icon-minus-circle"></use>
                   </svg>
                   <svg class="plus--icon">
-                    <use href="src/img/icons.svg#icon-plus-circle"></use>
+                    <use href="${icons}#icon-plus-circle"></use>
                   </svg>
                 </div>
                 <div class="bookmark--icon--container">
                   <svg class="bookmark--icon">
-                    <use href="src/img/icons.svg#icon-bookmark"></use>
+                    <use href="${icons}#icon-bookmark"></use>
                   </svg>
                 </div>
               </div>
@@ -108,14 +101,14 @@ const renderRecipe = async function (search) {
                 <h4>HOW TO COOK</h4>
                 <p>
                   This recipe was carefully designed and tested by
-                 <span class="recipe-source-id">${publisher}</span>. Please
+                 <span class="recipe-source-id">${recipe.publisher}</span>. Please
                   check out directions at their website.
                 </p>
-                <a href="${source}">
+                <a href="${recipe.source}">
                   <button class="btn go-to-btn">
                     DIRECTIONS
                     <svg class="arrow--icon">
-                      <use href="src/img/icons.svg#icon-arrow-right"></use>
+                      <use href="${icons}#icon-arrow-right"></use>
                     </svg>
                   </button>
                 </a>
@@ -126,6 +119,7 @@ const renderRecipe = async function (search) {
     recipeContainer.insertAdjacentHTML('afterbegin', html);
   } catch (err) {
     alert(err.message);
+    console.error(err);
   }
 };
 
---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+// import { async } from 'regenerator-runtime';
+export const state = {
+  apiKey: '5b0f13db-34ac-41da-941f-76dbebc41504',
+};
+
+export async function loadRecipe(id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=${state.apiKey}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      time: recipe.cooking_time,
+      img: recipe.image_url,
+      source: recipe.source_url,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+}
+console.log(state.recipe);
